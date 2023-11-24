5e7efe8d6a2cd145ce63239ad76aadcf790815ff
luispipe <lflondonor@unal.edu.co>
2023-11-24 06:47:10 -0500

    V6 Commit

---
+++ app/src/main/java/com/example/ahorravoltios_01/MoreStatisticsActivity.java
@@ -0,0 +1,14 @@
+package com.example.ahorravoltios_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoreStatisticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_statistics);
+    }
+}
